c74c0d91c6200da29857629a9b988711d34620a6
Nathan Hipps <nathanhipps@gmail.com>
2024-03-17 14:37:29 -0400

    wip

--- resources/views/livewire/parts/index.blade.php
+++ resources/views/livewire/parts/index.blade.php
@@ -1,20 +1,26 @@
-<div class="md:flex pt-12">
-    <x-parts.sidebar :categories="$categories"/>
-    <div>
-        <div class="border-b border-gray-100 px-5 sm:pl-8 sm:pr-0 pb-8">
-            <div class="sm:flex items-center sm:space-x-4">
-                <x-input
-                    wire:model.live.debounce="search"
-                    placeholder="Search for parts..."
-                    class="sm:w-96 w-full"
-                />
-                <x-button wire:click="clearSearch" class="w-full mt-4 sm:mt-0 sm:w-36">Clear Search</x-button>
+<div>
+
+    <div class="md:flex pt-12">
+        <x-parts.sidebar :categories="$categories"/>
+        <div>
+            <div class="border-b border-gray-100 px-5 sm:pl-8 sm:pr-0 pb-8">
+                <div class="sm:flex items-center sm:space-x-4">
+                    <x-input
+                        wire:model.live.debounce="search"
+                        placeholder="Search for parts..."
+                        class="sm:w-96 w-full"
+                    />
+                    <x-button wire:click="clearSearch" class="w-full mt-4 sm:mt-0 sm:w-36">Clear Search</x-button>
+                </div>
+            </div>
+            <x-parts>
+                @foreach($parts as $part)
+                    <x-parts.part :wire:key="$part->id" :part="$part"/>
+                @endforeach
+            </x-parts>
+            <div class="py-6 px-8">
+                {{ $parts->links() }}
             </div>
         </div>
-        <x-parts>
-            @foreach($parts as $part)
-                <x-parts.part :wire:key="$part->id" :part="$part"/>
-            @endforeach
-        </x-parts>
     </div>
 </div>
